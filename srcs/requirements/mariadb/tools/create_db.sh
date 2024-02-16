c0d44f035bb5ade2dff18ab265a3aef95cfd5383
Julen Girard <jgirard@jgirard.jgirard>
2024-02-16 06:37:31 -0500

    Makefile pending

--- srcs/requirements/mariadb/tools/create_db.sh
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -10,12 +10,14 @@ CREATE DATABASE $DB_NAME;
 CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
+USE $DB_NAME;
 eof
+    cat /tmp/create_db.sql dump.sql > /tmp/sss.sql
     mariadbd --user=mysql &
     sleep 3
-    mariadb < /tmp/create_db.sql
-    printf '\n\n\nFLUSH TABLES;\n' >> dump.sql
-    echo "use $DB_NAME;" | cat - dump.sql | mariadb
+    mariadb < /tmp/sss.sql
+    #printf '\n\n\nFLUSH TABLES;\n' >> dump.sql
+    #echo "use $DB_NAME;" | cat - dump.sql | mariadb
     kill %1
     rm /tmp/create_db.sql
 fi
